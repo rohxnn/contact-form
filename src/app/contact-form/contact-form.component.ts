146abfb29c533d952918c57e1d0792f60a31a9dc
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-09 15:26:56 +0530

    feat(contact-form): add validation and save functionality

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { ContactModel } from '../model/contact.model';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+//model
+import { ContactModel } from '../model/contact.model';
+//component
+import { CommonValidationErrorComponent } from '../shared/common-validation-error/common-validation-error.component';
+//validator
+import { noWhitespaceValidator } from '../shared/custom-validator/nowhitespace.validators';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ ReactiveFormsModule, CommonModule ],
+  imports: [ ReactiveFormsModule, CommonModule, CommonValidationErrorComponent ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
@@ -24,16 +28,20 @@ export class ContactFormComponent implements OnInit {
 
   initContactRegistration() {
     this.contactForm = this.fb.group({
-      'first_name': [''],
+      'first_name': ['', [ Validators.required, noWhitespaceValidator ]],
       'last_name': [''],
-      'email': [''],
+      'email': ['', [ Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), noWhitespaceValidator ]],
       'phone': ['']
     });
   }
 
   onClickAddContact() {
-    this.contactIds.push(this.contactIds.length + 1);
-    this.contacts.push(this.contactForm.value);
+    if(this.contactForm.valid) {
+      this.contactIds.push(this.contactIds.length + 1);
+      this.contacts.push(this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
   }
 
   onClickSelectContact(index: number) {
@@ -42,6 +50,21 @@ export class ContactFormComponent implements OnInit {
       this.contactForm.patchValue(this.contacts[index]);
     } else {
       this.contactForm.reset();
+    }
+  }
+
+  onClickDelete(index: number) {
+    this.contactIds.splice(index - 1, 1);
+    this.contacts.splice(index,1);
+  }
+
+  onClickSave() {
+    const existingObject = this.contacts.some((data =>  data === this.contactForm.value));
+    if(this.contactForm.valid && !existingObject) {
+      this.contacts.push(this.contactForm.value);
+      console.log("contacts:", this.contacts);
+    } else {
+      this.contactForm.markAllAsTouched();
     }
   }
 }
