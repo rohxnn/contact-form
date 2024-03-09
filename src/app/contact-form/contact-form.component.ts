f23e4ec0630d7bb8bb0eee3095d1a952b58a5860
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-09 13:55:58 +0530

    feat(contact-form): add select contact function login

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { ContactModel } from '../model/contact.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent implements OnInit {
   contactForm: FormGroup;
+  contactIds: number[] = [1];
+  contacts: ContactModel[] = [];
+  selectedContact: number;
   constructor( private fb: FormBuilder) {}
 
   ngOnInit() {
@@ -24,5 +29,19 @@ export class ContactFormComponent implements OnInit {
       'email': [''],
       'phone': ['']
     });
+  }
+
+  onClickAddContact() {
+    this.contactIds.push(this.contactIds.length + 1);
+    this.contacts.push(this.contactForm.value);
+  }
+
+  onClickSelectContact(index: number) {
+    this.selectedContact = index;
+    if(this.contacts[index]) {
+      this.contactForm.patchValue(this.contacts[index]);
+    } else {
+      this.contactForm.reset();
+    }
   }
 }
