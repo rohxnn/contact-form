a50e08f97343ba85a555c35d49696f0e15f93bff
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-09 13:40:36 +0530

    feat(contact-form): add reactive form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  contactForm: FormGroup;
+  constructor( private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.initContactRegistration();
+  }
+
+  initContactRegistration() {
+    this.contactForm = this.fb.group({
+      'first_name': [''],
+      'last_name': [''],
+      'email': [''],
+      'phone': ['']
+    });
+  }
 }
