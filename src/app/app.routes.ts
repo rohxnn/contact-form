3d76bd919254538486b1cf9923fc62cc86862836
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-09 13:21:44 +0530

    feat(contact-form): add component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/contact-form', pathMatch: 'full'},
+  { path: 'contact-form', component: ContactFormComponent}
+];
